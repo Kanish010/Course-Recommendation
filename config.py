8497272644d5dc1a6f0ae1f4414adeec961c5eb0
Kanish Alluri <142972386+Kanish010@users.noreply.github.com>
2024-07-27 20:27:49 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://username:password@localhost/CourseRecommendationDB')
-SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
